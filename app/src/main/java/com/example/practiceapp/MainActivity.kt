5b1b97afb42249dafb6ee3f3a878476bbdbedd7f
William Van Uitert <82126044+SnowballGus@users.noreply.github.com>
2025-04-13 16:33:37 -0400

    did some messing around with base UI interface

--- app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practiceapp.ui.theme.PracticeAppTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+
+    //android studio equivalent of main() method
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PracticeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,10 +37,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Black) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp),
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
